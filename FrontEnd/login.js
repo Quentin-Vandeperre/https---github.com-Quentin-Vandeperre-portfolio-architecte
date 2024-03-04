83fbaffbb793f60dee81428004a4c8b4ddde5b18
Quentin-Vandeperre <quentin.vandeperre12@outlook.fr>
2024-03-04 09:33:12 +0100

    login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,45 +1,37 @@
-async function fetch() {
-    let form = document.querySelector("form")
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()                                  // permet d'attendre l'xécution de tout le code
-    const login = {
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value,
+const enteredEmail = document.getElementById("email")
+const enteredPassword = document.getElementById("password")
+const span = document.getElementById("span")
+
+let submit = document.getElementById("submit")
+submit.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (enteredEmail.value === "" || enteredPassword.value === "") {
+        console.log("de")
+        span.style.display = "block"
     }
-    const loginJson = JSON.stringify(login)
-    fetch('http://localhost:5678/api/users/login', {
-        method: "POST",
-        body: loginJson,
-        headers: { "Content-Type": "application/json" },
-    }).then((response)=>console.log(response))
-      .then ((data)=>{
-      console.log(data)
-      })
-    })
-    
-}
-
-fetch()
-
-// const enteredEmail = document.getElementById("email").value
-// const enteredPassword = document.getElementById("password").value
-
-// let form = document.querySelector("form")
-//     form.addEventListener("submit", (event) => {
-//     event.preventDefault()
-//     fetch('http://localhost:5678/api/users/login',{
-//         method: "POST",
-//         body : JSON.stringify({
-//             email: enteredEmail,
-//             password: enteredPassword,
-//         }),
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//     })
-//     .then((response)=> response.json())
-//     .then ((data)=> {
-//         console.log(data)
-//     } )
-// })
-
+    else {
+        span.style.display = "none"
+        fetch('http://localhost:5678/api/users/login',{
+            method: "POST",
+            body : JSON.stringify({
+                email: enteredEmail.value,
+                password: enteredPassword.value,
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then((response)=> response.json())
+        .then ((data)=> {
+            const token = localStorage.setItem('token', JSON.stringify(data));
+            const auth = JSON.parse(localStorage.getItem('token'));
+            // Si on a bien un token dans le localstorage, on redirige vers la page d'accueil
+            if (auth && auth.token) {
+                window.location = "index.html";
+            // Sinon on affiche le message d'eereur
+            } else {
+                span.style.display = "block";
+            }
+        })
+    }
+})
--- FrontEnd/style.js
+++ FrontEnd/style.js
@@ -79,3 +79,60 @@ function filter(element) {
     console.log(element)
 }
 
+/**************************************MODAL********************************************/
+
+let modal = null
+
+const openModal = function(e){
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute('href')) // on va donc recuper l'attribut de href soit #modal1
+    target.style.display = null // afin d'annuler le display none
+    target.removeAttribute('aria-hidden')
+    target.setAttribute('aria-modal','true')
+    modal = target
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.jsModalClose').addEventListener('click', closeModal)
+    modal.querySelector('.jsModalStop').addEventListener('click', stopPropagation)
+}
+
+const closeModal = function (e){
+    if(modal === null)return
+    e.preventDefault()
+    modal.style.display = "none" // afin d'annuler le display none
+    modal.setAttribute('aria-hidden','true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.jsModalClose').removeEventListener('click', closeModal)
+    modal.querySelector('.jsModalStop').removeEventListener('click', stopPropagation)
+    modal = null
+}
+
+const stopPropagation = function(e){
+    e.stopPropagation()
+}
+
+document.querySelectorAll(".jsModal").forEach(a=>{
+    a.addEventListener('click', openModal)
+})
+
+window.addEventListener('keydown', function (e){
+    if (e.key === "Escape" || e.key === "Esc") {
+    closeModal(e)
+    }
+})
+
+
+let delectProject = document.querySelector(".delectProject")
+
+
+async function appendPicture() {
+    const reponse = await fetch('http://localhost:5678/api/works');
+    const data = await reponse.json();
+    data.forEach(function(e) {
+        let img = document.createElement("img");
+        delectProject.appendChild(img)
+        img.src = e.imageUrl;
+    }
+)}
+
+appendPicture()
